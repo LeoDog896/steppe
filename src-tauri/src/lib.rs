694f731e1df792dd8dbf0af2ad26173dff1709da
Tristan F. <LeoDog896@hotmail.com>
2024-11-11 23:47:03 -0800

    feat: setup config, begin deno worker

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,17 +1,27 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use deno_runtime::deno_core::{ModuleSpecifier, FsModuleLoader};
+use deno_runtime::deno_fs::RealFs;
+use deno_runtime::deno_permissions::PermissionsContainer;
+use deno_runtime::permissions::RuntimePermissionDescriptorParser;
+use deno_runtime::worker::{MainWorker, WorkerOptions, WorkerServiceOptions};
 use portable_pty::{native_pty_system, CommandBuilder, PtyPair, PtySize};
+use std::fs::{create_dir_all, File};
 use std::{
-    io::{BufRead, BufReader, Read, Write},
-    process::exit,
-    sync::{
+    io::{BufRead, BufReader, Read, Write}, path::Path, process::exit, sync::{
         atomic::{AtomicBool, Ordering},
         Arc,
-    },
-    thread::{self},
+    }, thread::{self}, rc::Rc, path::PathBuf
 };
 
 use tauri::{async_runtime::Mutex as AsyncMutex, State};
+
+struct SubTerminal {
+    pty_pair: Arc<AsyncMutex<PtyPair>>,
+    writer: Arc<AsyncMutex<Box<dyn Write + Send>>>,
+    reader: Arc<AsyncMutex<BufReader<Box<dyn Read + Send>>>>,
+    has_terminal: AtomicBool,
+}
 
 struct AppState {
     pty_pair: Arc<AsyncMutex<PtyPair>>,
@@ -105,8 +115,60 @@ async fn async_resize_pty(rows: u16, cols: u16, state: State<'_, AppState>) -> R
         .map_err(|_| ())
 }
 
+fn get_config_dir() -> PathBuf {
+    Path::new(&std::env::var("XDG_CONFIG_HOME").unwrap_or(
+        Path::new(&std::env::var("HOME").unwrap()).join(".config").to_string_lossy().to_string()
+    )).join("steppe")
+}
+
+fn get_config_path() -> PathBuf {
+    get_config_dir().join("config.js")
+}
+
+fn write_default_config(path: &PathBuf) {
+    if let Some(parent) = path.parent() {
+        create_dir_all(parent).unwrap();
+    }
+
+    File::create_new(path).unwrap().write(b"export {}").unwrap();
+}
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
+    let path = get_config_path();
+    if !path.exists() {
+        write_default_config(&path);
+    }
+
+    // deno boilerplate from https://github.com/denoland/deno/blob/main/runtime/examples/extension/main.rs
+    let main_module = ModuleSpecifier::from_file_path(get_config_path()).unwrap();
+
+    let fs = Arc::new(RealFs);
+
+    let permission_desc_parser =
+        Arc::new(RuntimePermissionDescriptorParser::new(fs.clone()));
+
+    let mut worker = MainWorker::bootstrap_from_options(
+        main_module.clone(),
+        WorkerServiceOptions {
+            module_loader: Rc::new(FsModuleLoader),
+            permissions: PermissionsContainer::allow_all(permission_desc_parser),
+            blob_store: Default::default(),
+            broadcast_channel: Default::default(),
+            feature_checker: Default::default(),
+            node_services: Default::default(),
+            npm_process_state_provider: Default::default(),
+            root_cert_store_provider: Default::default(),
+            shared_array_buffer_store: Default::default(),
+            compiled_wasm_module_store: Default::default(),
+            v8_code_cache: Default::default(),
+            fs,
+        },
+        WorkerOptions {
+            ..Default::default()
+        },
+    );
+
     let pty_system = native_pty_system();
 
     let pty_pair = pty_system
